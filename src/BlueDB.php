9e4a24b23ed6d6ff222326dc140b4c5cb7411598
GREGA-AW\grega <grega@mohorko.info>
2017-03-14 16:41:48 +0100

    Created the project in NetBeans.

---
+++ src/BlueDB.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * BlueDB.php
+ * 
+ * Includes all files needed by this library.
+ * 
+ * @project BlueDB
+ * @author Grega Mohorko <grega@mohorko.info>
+ * @copyright Mar 14, 2017 Grega Mohorko
+ */
+
